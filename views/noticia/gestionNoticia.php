683fb012619afa24f2ad4be2d951f095533c438a
Diego Faundez <dfaundez@s4t.page>
2021-05-21 16:09:14 -0400

    Se elimino modulo de noticias por que no es requerido en la evaluacion

--- views/noticia/gestionNoticia.php
+++
@@ -1,3 +0,0 @@
-<div class="container text-center">
-    <h1>HOLA GESTION</h1>
-</div>
--- views/noticia/noticia.php
+++
@@ -1,4 +0,0 @@
-<div class="container text-center">
-    <h1>NOTICIAS DE ULTIMO MINUTO</h1>
-    <a class="btn btn-primary" href="<?=base_url?>PrivateNoticia/index">GESTION</a>
-</div>
